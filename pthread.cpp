6104eb701b5641e72506396ab3159609f7b19a86
Gabriel Salmon <gtsalmon@gmail.com>
2021-10-08 20:39:21 -0400

    FINALLY

--- pthread.cpp
+++ pthread.cpp
@@ -26,10 +26,14 @@ void* MST(void* vertexes) {
     selected[0] = true;
     // std::cout<<"made it to e\tselected 0, 1: "<<selected[0]<<"\t"<<selected[1]<<std::endl;
     //building our MST
-
-    // for (int i = 0; i <verts.size(); i++) {
-    //     std::cout<<"in MST: name: "<<verts[i]->name<<std::endl;
-    // }
+    float min_in_sector = 1.1; // minimum weight for this thread
+    int min_index;
+    for (int i = 0; i <verts.size(); i++) {
+        if (verts[i]->weight < min_in_sector) {
+            min_in_sector = verts[i]->weight;
+            min_index = i;
+        }
+    }
     // std::cout<<"made it to e"<<std::endl;
     while (edges.size() < verts.size() - 1) {
         float minimum = 1.1; // each probability product's max is 1
@@ -75,8 +79,8 @@ void* MST(void* vertexes) {
     //         std::cout<<"\tchildren["<<j<<"]: "<<verts[i]->children[j]->name<<std::endl;
     //     }
     // }
-    // std::cout<<"made it to f"<<std::endl;
-    pthread_exit(0);
+    // std::cout<<"min_index (MST): "<<min_index<<std::endl;
+    return (void*) min_index;
 }
 
 void traverse(Vertex* vert) { // traverse and print from passed vertex through tree
@@ -86,9 +90,44 @@ void traverse(Vertex* vert) { // traverse and print from passed vertex through t
     }
 }
 
-void hamiltonian(Vertex* vert) { // helper for traverse
-    traverse(vert);
-    std::cout<<vert->name<<std::endl;
+void hamiltonian(std::vector<Vertex*>* vert_cuts, std::vector<int> mins, int n_sectors) {
+    // std::cout<<"n_sectors: "<<n_sectors<<std::endl;
+    
+    for (int i = 1; i < n_sectors; i++) { // linking sectors by their minimum-weighted vertexes
+        // std::cout<<"at sector "<<i<<std::endl;
+        // std::cout<<"\tsize of cut: "<<vert_cuts[i].size()<<std::endl;
+        for (int j = 0; j < vert_cuts[i].size(); j++) {
+            // std::cout<<"\tat element "<<j<<std::endl;
+            if (vert_cuts[i][j]->parent == nullptr) {
+                // std::cout<<"\t\tfound an orphan. name: "<<vert_cuts[i][j]->name<<std::endl;
+                // std::cout<<"\t\ti % (int)sqrt(n_sectors): "<<i % (int)sqrt(n_sectors)<<std::endl;
+                if (i % (int)sqrt(n_sectors) > 0 ) { // trying to link proximal sectors
+                    // std::cout<<"\t\tmins["<<i<<" - 1]: "<<mins[i - 1]<<std::endl;
+                    vert_cuts[i][j]->parent = vert_cuts[i - 1][mins[i - 1]];
+                    vert_cuts[i - 1][mins[i - 1]]->children.push_back(vert_cuts[i][j]);
+                } else {
+                    // std::cout<<"\t\tmins["<<i<<" - (int)sqrt(n_sectors)]: "<<mins[i - (int)sqrt(n_sectors)]<<std::endl;
+                    vert_cuts[i][j]->parent = vert_cuts[i - (int)sqrt(n_sectors)][mins[i - (int)sqrt(n_sectors)]];
+                    vert_cuts[i - (int)sqrt(n_sectors)][mins[i - (int)sqrt(n_sectors)]]->children.push_back(vert_cuts[i][j]);
+                }
+                break;
+            }
+        }
+    }
+    // for (int i = 0; i < n_sectors; i++) {
+    //     for (int j = 0; j < vert_cuts[i].size(); j++) {
+    //         std::cout<<"vert_cut["<<i<<"]: "<<std::endl;
+    //         std::cout<<"\tvertex["<<j<<"]: "<<std::endl;
+    //         std::cout<<"\tname: "<<vert_cuts[i][j]->name<<std::endl;
+    //         if (vert_cuts[i][j]->parent) {std::cout<<"\tparent: "<<vert_cuts[i][j]->parent->name<<std::endl;}
+    //         for (int k = 0; k < vert_cuts[i][j]->children.size(); k++) {
+    //             std::cout<<"\tchild["<<k<<"]: "<<vert_cuts[i][j]->children[k]->name<<std::endl;
+    //         }
+    //     }
+    // }
+    std::cout<<"Start traversal"<<std::endl;
+    // vert_cuts[0][0] has no parent, must start there
+    traverse(vert_cuts[0][0]);
 }
 
 int main(int argc, char *argv[]) {
@@ -123,6 +162,8 @@ int main(int argc, char *argv[]) {
             }
         }
     }
+    std::vector<int> min_from_sector;
+    void* min;
     pthread_t* threads;
     threads = (pthread_t*)malloc(n_threads * sizeof(pthread_t));
     // std::cout<<"vert_cuts[0][1].name: "<<vert_cuts[0][1]->name<<std::endl;
@@ -131,19 +172,24 @@ int main(int argc, char *argv[]) {
         pthread_create(&threads[i], NULL, MST, (void*) &vert_cuts[i]);
     }
     for (int i = 0; i < n_threads; i++) {
-        pthread_join(threads[i], NULL);
+        pthread_join(threads[i], &min);
+        min_from_sector.push_back(reinterpret_cast<long>(min));
+        // std::cout<<"min_index of sector i (main): "<<reinterpret_cast<long>(min)<<std::endl;
     }
     // std::cout<<"made it to g"<<std::endl;
     free(threads);
 
-    for (int i = 0; i < vertexes.size(); i++) {
-        std::cout<<"vertexes["<<i<<"]: "<<std::endl;
-        std::cout<<"\tname: "<<vertexes[i].name<<std::endl;
-        if (vertexes[i].parent) {std::cout<<"\tparent: "<<vertexes[i].parent->name<<std::endl;}
-        for (int j = 0; j <vertexes[i].children.size(); j++) {
-            std::cout<<"\tchild["<<j<<"]: "<<vertexes[i].children[j]->name<<std::endl;
-        }
-    }
+    // for (int i = 0; i < vertexes.size(); i++) {
+    //     std::cout<<"vertexes["<<i<<"]: "<<std::endl;
+    //     std::cout<<"\tname: "<<vertexes[i].name<<std::endl;
+    //     if (vertexes[i].parent) {std::cout<<"\tparent: "<<vertexes[i].parent->name<<std::endl;}
+    //     for (int j = 0; j <vertexes[i].children.size(); j++) {
+    //         std::cout<<"\tchild["<<j<<"]: "<<vertexes[i].children[j]->name<<std::endl;
+    //     }
+    // }
     // std::cout<<"made it to h"<<std::endl;
-    hamiltonian(&vertexes[0]);
+    hamiltonian(vert_cuts, min_from_sector, n_threads);
+
+    std::cout<<"Finished!"<<std::endl;
+    return 0;
 }
